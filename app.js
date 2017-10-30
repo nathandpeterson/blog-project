e69ba6a2936ccf3d1e03a093decad6abb0126c80
Nathan Daniel Peterson <natperson@gmail.com>
2017-10-30 11:10:08 -0700

    serve data to html

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const SERVER = 'http://localhost:3000/posts'
 
+function display(data){
+  let container = document.querySelector('.container')
+  container.innerHTML = data[0].content
+}
+
 axios.get(SERVER)
-  .then(res => console.log(res))
+  .then(res => {
+    console.log(res.data)
+    display(res.data)
+  })
   .catch(err => console.log(err))

2e35188b9acfeba2a231b8f9f4ea7c4a027b973d
Nathan Daniel Peterson <natperson@gmail.com>
2017-10-30 11:28:01 -0700

    set up show all

--- app.js
+++ app.js
@@ -2,12 +2,31 @@ const SERVER = 'http://localhost:3000/posts'
 
 function display(data){
   let container = document.querySelector('.container')
-  container.innerHTML = data[0].content
+  container.innerHTML = `<h3>${data[0].title}</h3>
+  <p>${data[0].content}</p>
+  <h5>${data[0].created}</h5>`
+}
+
+function showAll(data){
+  let container = document.querySelector('.container')
+  for(let i = 0; i < data.length; i++){
+    container.innerHTML += `<h3>${data[i].title}</h3>
+    <p>${data[i].content}</p>
+    <h5>${data[i].created}</h5>`
+  }
+}
+
+//where to call this?
+function showOne(post){
+  let container = document.querySelector('.container')
+    container.innerHTML += `<h3>${data.title}</h3>
+    <p>${data.content}</p>
+    <h5>${data.created}</h5>`
 }
 
 axios.get(SERVER)
   .then(res => {
-    console.log(res.data)
-    display(res.data)
+    // display(res.data)
+    showAll(res.data)
   })
   .catch(err => console.log(err))
